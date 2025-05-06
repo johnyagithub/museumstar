977331cf0daecac83093104b6e6ad49566ca8b1b
johnyagithub <jonya1530@gmail.com>
2025-05-06 10:13:24 +0700

    5-5-68

--- inc/inc-footer.php
+++ inc/inc-footer.php
@@ -1,5 +1,18 @@
 <div class="box-passport mt-auto">
-  MUSEUM PASSPORT
+  <div class="container-lg">
+    <div class="row">
+      <div class="col-md-4">
+        dsfds
+      </div>
+      <div class="col-md-8 text-center">
+        <h1>MUSEUM</h1>
+        <h3>PASSPORT</h3>
+        <p class="m-0">แคมเปญพิเศษ สำหรับนักท่องเที่ยวสายมิวเซียม สะสมตราประทับ MUSEUM PASSPORT ครบ 5 แห่ง</p>
+        <p class="m-0">ลุ้นรับของที่ระลึก <span class="text-warning">จำนวน 200 รางวัล ตั้งแต่วันนี้ - 30 กันยายน 2568</span></p>
+        <a href="#" class="btn btn-warning mt-4" download><img src="./public/images/icon-pdf.png" alt=""> <u>ดาวน์โหลด.. PASSPORT</u></a>
+      </div>
+    </div>
+  </div>
 </div>
 <footer>
   <div class="container py-5">
