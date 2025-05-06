9d863223e10efff5c7e684a4d74b5aa4437b727d
johnyagithub <jonya1530@gmail.com>
2025-05-06 10:56:29 +0700

    img

--- inc/inc-footer.php
+++ inc/inc-footer.php
@@ -1,26 +1,32 @@
 <div class="box-passport mt-auto">
   <div class="container-lg">
-    <div class="row">
-      <div class="col-md-4">
-        dsfds
-      </div>
-      <div class="col-md-8 text-center">
-        <h1>MUSEUM</h1>
-        <h3>PASSPORT</h3>
-        <p class="m-0">แคมเปญพิเศษ สำหรับนักท่องเที่ยวสายมิวเซียม สะสมตราประทับ MUSEUM PASSPORT ครบ 5 แห่ง</p>
-        <p class="m-0">ลุ้นรับของที่ระลึก <span class="text-warning">จำนวน 200 รางวัล ตั้งแต่วันนี้ - 30 กันยายน 2568</span></p>
-        <a href="#" class="btn btn-warning mt-4" download><img src="./public/images/icon-pdf.png" alt=""> <u>ดาวน์โหลด.. PASSPORT</u></a>
+    <div class="b-passport">
+      <div class="row m-0 align-items-center justify-content-center">
+        <div class="col-md-4 p-0">
+          <img src="./public/images/img-passport.png" class="img-passport" alt="">
+        </div>
+        <div class="col-md-8 px-lg-5 p-3 pr-md-5 text-center">
+          <h1>MUSEUM</h1>
+          <h3>PASSPORT</h3>
+          <br>
+          <p class="m-0">แคมเปญพิเศษ สำหรับนักท่องเที่ยวสายมิวเซียม สะสมตราประทับ MUSEUM PASSPORT ครบ 5 แห่ง</p>
+          <p class="m-0">ลุ้นรับของที่ระลึก <span class="text-warning">จำนวน 200 รางวัล ตั้งแต่วันนี้ - 30 กันยายน 2568</span></p>
+          <br>
+          <a href="#" class="btn btn-warning mt-4" download><img src="./public/images/icon-pdf.png" alt=""> <u>ดาวน์โหลด.. PASSPORT</u></a>
+        </div>
       </div>
     </div>
   </div>
 </div>
 <footer>
-  <div class="container py-5">
-    ghjkgk
+  <div class="container py-4">
+    <b>จัดทำโดย</b>
+    <hr style="max-width: 270px;">
+    <b>ติดตามช่องทางเพิ่มเติม</b>
   </div>
-  <div class="footer-Copyright" style="border-top-color: #1E1F1F;">
+  <div class="footer-Copyright">
     <div class="container">
-      <p class="py-3 m-0" style="font-size: 12px;color:#797A7A;">© Copyright 2016 Museum Thailand, All right Reserved.</p>
+      <p class="py-1 m-0" style="font-size: 12px;color:#797A7A;">© Copyright 2016 Museum Thailand, All right Reserved.</p>
     </div>
   </div>
 </footer>
